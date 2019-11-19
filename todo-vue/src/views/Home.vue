173c7ecf5bfe1e08b051347128acc17c238cfcd5
dmswjd9501 <dmswjd950101@gmail.com>
2019-11-19 11:54:57 +0900

    Vue | getTodos with JWT header

--- todo-vue/src/views/Home.vue
+++ todo-vue/src/views/Home.vue
@@ -49,7 +49,14 @@ export default {
     },
     getTodos() {
       //axios 요청
-      axios.get('http://127.0.0.1:8000/api/v1/todos/')
+      this.$session.start()
+      const token = this.$session.get('jwt')
+      const options = {
+        headers : {
+          Authorization: `JWT ${token}`
+        }
+      }
+      axios.get('http://127.0.0.1:8000/api/v1/todos/', options)
         .then(response => {
           console.log(response) // 만약, 오류가 발생하게 되면 ESLint 설정을 package.json을 추가!
           this.todos = response.data
